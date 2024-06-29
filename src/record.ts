131826fe9640ae910f5e77879cdfbb12d7119487
lvboda <lv_boda@163.com>
2024-06-30 00:37:35 +0800

    fix: readRecord error.

--- src/record.ts
+++ src/record.ts
@@ -11,19 +11,26 @@ type Record = {
     response: AxiosResponse;
     image?: Image;
 };
-
+// TODO 这里设计的有问题 频繁读写文件会导致性能问题
+// 简单加个catch解决类似错误 Error: ENOENT: no such file or directory, open 'xxx'
 export function readRecord() {
-    return JSON.parse(fs.readFileSync(RECORD_FILE_PATH, { encoding: "utf8" })) as Record[];
+    try {
+        return JSON.parse(fs.readFileSync(RECORD_FILE_PATH, { encoding: "utf8" })) as Record[];
+    } catch {
+        return [];
+    }
 }
 
 export function writeRecord(response: AxiosResponse, image?: Image, maxStorageCount = 500) {
-    delete response.request;
+    try {
+        delete response.request;
 
-    let records = readRecord();
-    image && (records = records.filter((item) => (item.image && image && !isEqual(item.image, image))));
-
-    records.length > maxStorageCount && records.shift();
+        let records = readRecord();
+        image && (records = records.filter((item) => (item.image && image && !isEqual(item.image, image))));
     
-    records.push({ time: new Date(), response, image });
-    fs.writeFileSync(RECORD_FILE_PATH, JSON.stringify(records), { encoding: "utf8" });
+        records.length > maxStorageCount && records.shift();
+        
+        records.push({ time: new Date(), response, image });
+        fs.writeFileSync(RECORD_FILE_PATH, JSON.stringify(records), { encoding: "utf8" });
+    } catch {}
 }
